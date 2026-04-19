6639499f86bf96d70c0933e4245e0ffbf2701081
domesya <163451587+domesya@users.noreply.github.com>
2026-04-19 19:02:21 +0200

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -10,7 +10,7 @@ export ADD_HOOKS="self-updater.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
 export ICON=/usr/share/icons/hicolor/256x256/apps/org.contourterminal.Contour.png
 export DESKTOP=/usr/share/applications/org.contourterminal.Contour.desktop
-
+export URUNTIME_PRELOAD=1
 # Deploy dependencies
 quick-sharun /usr/sbin/contour
 
